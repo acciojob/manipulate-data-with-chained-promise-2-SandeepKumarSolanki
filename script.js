21e07bd5f1ea692e86e11bb7caa8881ff553a1ef
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-02-15 09:02:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+
+let arr = [1,2,3,4];
+let ouput = document.getElementById('ouput');
+
+new Promise((resolve , reject) => {
+	return resolve(arr);
+})
+.then((arr) =>{
+	let even = arr.filter((val) => val % 2 == 0);
+	setTimeout((even)=>{
+		ouput.innerText = `${even}`;
+		return resolve(even);
+	},1000)
+}).then((data)=>{
+	setTimeout(()=> {
+		let afterMulti = data.map((val)=> val*2);
+		ouput.innerText = `${afterMulti}`
+	},2000)
+}).catch(err => console.log(err));
+
+
+
+
+
+
